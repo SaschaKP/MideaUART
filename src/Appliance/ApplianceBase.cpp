50c880f89952147b9a53dde29aa09308b9efc6ea
Sascha <k.puligheddu@alice.it>
2025-10-01 23:12:10 +0200

    Update ApplianceBase.cpp

--- src/Appliance/ApplianceBase.cpp
+++ src/Appliance/ApplianceBase.cpp
@@ -1,9 +1,9 @@
 #include "Appliance/ApplianceBase.h"
 #include "Helpers/Log.h"
-#ifdef ARDUINO_ARCH_ESP32
+//#ifdef ARDUINO_ARCH_ESP32
 #include <WiFi.h>
-#else
-#include <ESP8266WiFi.h>
+//#else
+//#include <ESP8266WiFi.h>
 #endif
 
 namespace dudanov {
